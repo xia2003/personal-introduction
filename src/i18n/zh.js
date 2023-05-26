2060352f8270e4bffb2ba8000cdc381162b9b831
xia2003 <58935036+xia2003@users.noreply.github.com>
2023-05-26 20:45:39 +0800

    Update zh.js

--- src/i18n/zh.js
+++ src/i18n/zh.js
@@ -1,44 +1,44 @@
 export default {
-  nav: {
-    name: "霍世苍",
-    home: "主页",
-    about: "关于",
-    seniority: "资历",
-    skills: "技能",
-    project: "项目",
-    blog: "博客",
-    contact: "联系方式",
-  },
-  home: {
-    string: "你好，欢迎来到我的网站！",
-  },
-  about: {
-    imgSrc: "http://www.netbian.com/down.php?id=30961&type=undefined",
-    imgAlt: "图片消失啦",
-    me: "关于我",
-    introduce: "简单介绍",
-    describe:
-      "Web前端开发人员，具有丰富的知识和经验，从事Web技术，技术崇拜，喜欢折腾，能提供高质量的工作。",
-    ageNumber: "22",
-    age: "年龄",
-    experience: "工作经验",
-    experienceNumber: "0",
-  },
-  seniority: {
-    seniority: "资历",
-    experience: "个人经历",
-  },
-  skills: {
-    skills: "技能",
-    level: "技能水平",
-  },
-  project: {
-    project: "项目",
-    Alltheitems: "所有项目",
-  },
-  contact: {
-    qq: "QQ",
-    Phone: "手机号",
-    Email: "邮箱",
-  },
-};
+      nav: {
+        name: "夏逸冰",
+        home: "主页",
+        about: "关于",
+        seniority: "资历",
+        skills: "技能",
+        project: "项目",
+        blog: "博客",
+        contact: "联系方式",
+      },
+      home: {
+        string: "你好，欢迎来到我的网站！",
+      },
+      about: {
+        imgSrc: "http://www.netbian.com/down.php?id=30961&type=undefined",
+        imgAlt: "图片消失啦",
+        me: "关于我",
+        introduce: "简单介绍",
+        describe:
+          "Web前端开发人员，具有丰富的知识和经验，从事Web技术，技术崇拜，喜欢折腾，能提供高质量的工作。",
+        ageNumber: "20",
+        age: "年龄",
+        experience: "工作经验",
+        experienceNumber: "0",
+      },
+      seniority: {
+        seniority: "资历",
+        experience: "个人经历",
+      },
+      skills: {
+        skills: "技能",
+        level: "技能水平",
+      },
+      project: {
+        project: "项目",
+        Alltheitems: "所有项目",
+      },
+      contact: {
+        qq: "QQ",
+        Phone: "手机号",
+        Email: "邮箱",
+      },
+    }; 
