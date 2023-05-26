b84cfa8c5de488ea1a06c427f8aba0638f9cf341
xia2003 <58935036+xia2003@users.noreply.github.com>
2023-05-26 21:27:44 +0800

    Update en.js

--- src/i18n/en.js
+++ src/i18n/en.js
@@ -1,44 +1,44 @@
 export default {
-      nav: {
-        name: "XiaYibing",
-        home: "home",
-        about: "about",
-        seniority: "seniority",
-        skills: "skills",
-        project: "project",
-        blog: "blog",
-        contact: "contact",
-      },
-      home: {
-        string: "Hi, Welcome to my website!",
-      },
-      about: {
-        imgSrc: "http://www.netbian.com/down.php?id=30961&type=undefined",
-        imgAlt: "Image disappeared",
-        me: "About me",
-        introduce: "A simple introduction",
-        describe:
-          "Web front-end developer, has a wealth of knowledge and experience, engaged in Web technology, technology, worship, love, can provide the high quality of work.",
-        ageNumber: "20",
-        age: "Age",
-        experience: "Work experience",
-        experienceNumber: "0",
-      },
-      seniority: {
-        seniority: "Seniority",
-        experience: "Personal experience",
-      },
-      skills: {
-        skills: "Skills",
-        level: "Skill levels",
-      },
-      project: {
-        project: "project",
-        Alltheitems: "All the items",
-      },
-      contact: {
-        qq: "QQ",
-        Phone: "Phone",
-        Email: "Email",
-      },
-    };
+  nav: {
+    name: "XiaYibing",
+    home: "home",
+    about: "about",
+    seniority: "seniority",
+    skills: "skills",
+    project: "project",
+    blog: "blog",
+    contact: "contact",
+  },
+  home: {
+    string: "Hi, Welcome to my website!",
+  },
+  about: {
+    imgSrc: "http://www.netbian.com/down.php?id=30961&type=undefined",
+    imgAlt: "Image disappeared",
+    me: "About me",
+    introduce: "A simple introduction",
+    describe:
+      "Web front-end developer, has a wealth of knowledge and experience, engaged in Web technology, technology, worship, love, can provide the high quality of work.",
+    ageNumber: "20",
+    age: "Age",
+    experience: "Work experience",
+    experienceNumber: "0",
+  },
+  seniority: {
+    seniority: "Seniority",
+    experience: "Personal experience",
+  },
+  skills: {
+    skills: "Skills",
+    level: "Skill levels",
+  },
+  project: {
+    project: "project",
+    Alltheitems: "All the items",
+  },
+  contact: {
+    qq: "QQ",
+    Phone: "Phone",
+    Email: "Email",
+  },
+};
